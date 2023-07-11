a6e98caaa83f217e81cc4900c2e601397fcfc59d
AKSUF <abu053125@gmail.com>
2023-07-11 19:00:37 +0600

    how to use construrctor in php

--- persion.php
+++ persion.php
@@ -2,13 +2,16 @@
 
 class persion{
     public $name,$age;
+    function __construct($name="No name",$age=0){
+        $this->name=$name;
+        $this->age=$age; 
+    }
     function show(){
         echo $this->name."</br>".$this->age;
     }
 }
-$p1=new persion();
-$p1->name="Yahoo Baba";
-$p1->age=20;
 
+$p1=new persion("Yahoo Baba",69);
+$p1=new persion();
 $p1->show();
 ?>
