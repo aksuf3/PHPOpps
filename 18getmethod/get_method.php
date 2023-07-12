f89528efb7def0ba05c653cded35251bb94aa6dc
AKSUF <abu053125@gmail.com>
2023-07-12 18:45:37 +0600

    how to access private method by get method

--- 18getmethod/get_method.php
+++ 18getmethod/get_method.php
@@ -1,12 +1,18 @@
 <?php
+
 class abc{
-private $name="Hello Yahoo bana";
-public function __get($property){
-echo "You are trying to access non existng or private property($property)";
+private $data=["name"=>"Yahoo Baba","course"=>"PHP","fee"=>"2000"];
+public function __get($key){
+if(array_key_exists($key,$this->data)){
+    return $this->data[$key];
+}else{
+return "This is key($key) is not defined";
+
 }
 }
+}
+
 $test=new abc();
-$test->name;
-
-
+echo $test->name;
+echo $test->course;
 ?>
