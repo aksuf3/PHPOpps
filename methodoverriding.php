3fa2ee258ce364ff35d76cc6a26062d32b36a80b
AKSUF <abu053125@gmail.com>
2023-07-12 13:15:49 +0600

    how to use traits

--- methodoverriding.php
+++ methodoverriding.php
@@ -1,36 +1,19 @@
 <?php
-trait hello
-{
-    public function sayhello()
-    {
-        echo "Helo my traits";
-    }
-}
-
-trait hi
-{
-    public function sayhello()
-    {
-        echo "hi my traits";
+trait Hello {
+    private function sayhello() {
+        echo "Hello my traits";
     }
 }
 
 
-class base
-{
-   use hello,hi{
-hello::sayhello insteadOf hi;
-hi::sayhello as newhello;
-   }
+class Base {
+    use Hello{
+        Hello::sayhello as public;
+       
+    }
 }
-// class child extends base{
-//     use hello;
-//     public function sayhello()
-//     {
-//         echo "Helo my child";
-//     }
-// }
 
-$test = new base();
+$test = new Base();
 $test->sayhello();
-$test->newhello();
+
+?>
