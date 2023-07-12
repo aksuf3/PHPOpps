a340aefafd7fcee6ef38219348ebf2768623669d
AKSUF <abu053125@gmail.com>
2023-07-12 20:47:01 +0600

    fhfdhfdgdgsd

--- 20callmethod/call.php
+++ 20callmethod/call.php
@@ -3,15 +3,24 @@ class student
 {
     private $first_name;
     private $last_name;
+
     private function setName($fname,$lname){
         $this->first_name=$fname;
         $this->last_name=$lname;
     }
+
     public function __call($method, $arguments)
     {
-        echo "This is private or Non existing method:$method";
-        print_r($arguments);
+      if(method_exists($this,$method)){
+call_user_func_array([$this,$method],$arguments);
+      }else{
+echo "Method does not exists:$method";
+      };
     }
 }
+
 $test=new student();
 $test->setName("Yahoo","Baba");
+echo "<pre>";
+print_r($test);
+echo "</pre>";
