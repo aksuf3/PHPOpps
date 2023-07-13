3c2bacabaadb9ba1798ac524333bd0e2ebe50ed6
AKSUF <abu053125@gmail.com>
2023-07-13 17:34:15 +0600

    how to use condition funruio

--- 30conditional/condition.php
+++ 30conditional/condition.php
@@ -3,8 +3,20 @@ interface MyInterface{
 
 }
 class MyClass{
-
+public $test;
 }
+trait Mytrait{
+    public function Mymethod(){
+
+    }
+}
+
+if(trait_exists('Mytrait')){
+echo "This trait is exists";
+}else{
+    echo "This trait is not exists";
+}
+
 
 //interface related
 if(interface_exists('MyInterface')){
@@ -21,6 +33,20 @@ if(class_exists('MyClass')){
 echo"This class is exists";
 }else{
     echo"This class is not  exists";
+}
+
+if(property_exists('MyClass','test')){
+    echo "This Property is exists";
+
+}else{
+    echo "This propety does not eists";
+}
+
+$obj=new MyClass();
+if(is_a($obj,'MyClass')){
+    echo "This  Object is of clss myclass";
+}else{
+    echo "tThis object is not of Class My Class";
 }
 
 ?>
